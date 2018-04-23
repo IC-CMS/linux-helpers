ac6210dd1f418ceeddf2434d04f2b3d2a447cec7
David Hessler <davidh.092705@gmail.com>
2018-04-23 11:21:29 -0400

    Update InstallDocker.sh

--- scripts/InstallDocker.sh
+++ scripts/InstallDocker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo yum update
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce
+sudo yum install -y docker
 sudo usermod -a -G docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/bin/docker-compose > /dev/null
 sudo chmod +x /usr/bin/docker-compose
